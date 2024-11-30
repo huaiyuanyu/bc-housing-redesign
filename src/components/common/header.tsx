76824861d2c545e0039544e533a7bee95c635875
Gary Yu <hyu100@my.bcit.ca>
2024-11-29 17:00:52 -0800

    small change made to img src in header component in order to have it function when deployed

--- src/components/common/header.tsx
+++ src/components/common/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
     <div className="bg-gradient-to-r from-lime-300 to-sky-300 pt-1">
       <div className="bg-secondary p-2">
         <Link href="/">
-          <img src="/images/BCHousingHeaderLogo.png" alt="BC Housing" />
+          <img src="/images/BCHousingHeaderLogo.PNG" alt="BC Housing" />
         </Link>
       </div>
     </div>
